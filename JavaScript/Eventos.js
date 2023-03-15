1bfdbe9993b9f9290603274ac775f4871259edaa
Marcos <perezcamposmarcos@gmail.com>
2023-03-15 16:29:03 -0300

    Proyecto Final

--- JavaScript/Eventos.js
+++ JavaScript/Eventos.js
@@ -1,31 +1,23 @@
 
-let contrato = prompt("Si le gustaria contratarnos para eventos escriba Si de lo contrario continue: ")
-if (contrato == "Si") {
-   alert("Perfecto, calculemos los costos")
-
-    const sumar = (a,b) => {
-        return a + b
-     };
-    
-    const restar = (a,b) =>{
-       return a - b
-    };
-    
-    const calcularIva = x => x * 0.21
-    
-    let precioEvento = 5000
-    let precioDescuento = 500
-    
-    let iva = calcularIva(precioEvento)
-    let precioConIva = sumar(precioEvento,iva)
-    let precioFinal = restar (precioConIva, precioDescuento)
-    
-    alert ("El precio final incluyendo el precio +iva + descuento es: "+ precioFinal)
-
-} else {
-    alert("Bueno para la proxima sera, mire nuestro portafolio")
-
-    console.log(contrato)
-}
+Swal.fire({
+    title: 'Necesita informacion para algun evento?',
+    showDenyButton: true,
+    showCancelButton: true,
+    confirmButtonText: 'Yes',
+    denyButtonText: 'No',
+    customClass: {
+      actions: 'my-actions',
+      cancelButton: 'order-1 right-gap',
+      confirmButton: 'order-2',
+      denyButton: 'order-3',
+    }
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire('Tenemos un formulario por si necesita contactarse!', '', 'info')
+    } else if (result.isDenied) {
+      Swal.fire('Okey, mire nuestras fotos mientras', '', 'success')
+    }
+  })
+  
 
 
